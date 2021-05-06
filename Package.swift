4f49ddec7a735af64ba737a03e82dc48388df81b
Kamil Modzelewski <kammodze@gmail.com>
2021-05-07 00:32:43 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyCodable",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "SwiftyCodable", targets: ["SwiftyCodable"])
+    ],
+    targets: [
+        .target(name: "SwiftyCodable", dependencies: [], path: "SwiftyCodable", resources: [])
+    ]
+)
